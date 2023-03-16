779a59bd59728ff6384967b5960ea7e975e8bee1
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-16 12:40:51 +0530

    Initial commit

---
+++ FrameWorks/FrameWorks/FrameWorks.h
@@ -0,0 +1,18 @@
+//
+//  FrameWorks.h
+//  FrameWorks
+//
+//  Created by IOS on 16/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameWorks.
+FOUNDATION_EXPORT double FrameWorksVersionNumber;
+
+//! Project version string for FrameWorks.
+FOUNDATION_EXPORT const unsigned char FrameWorksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameWorks/PublicHeader.h>
+
+
---
+++ FrameWorks/Serv.swift
@@ -0,0 +1,15 @@
+//
+//  Serv.swift
+//  FrameWorks
+//
+//  Created by IOS on 16/03/23.
+//
+
+import Foundation
+
+public class Serv {
+    public init() {}
+    public func writeMsg() -> String {
+        return "Hello there"
+    }
+}
